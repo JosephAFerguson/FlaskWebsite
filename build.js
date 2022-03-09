3619bf7fd86ffec13c7ea738a2bdcb13306a8b9e
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2022-03-09 12:13:23 -0500

    new animations

--- build.js
+++ build.js
@@ -1,19 +1,19 @@
 var nav = document.querySelectorAll(".navbar");
 var count = 0;
 $("nav").each(function(i){
-    this.style.opacity = "0";
+    $(this).slideUp(0.1);
 });
 function visi(){
     if(count == 0){
         $("nav").each(function(i){
-            $(this).animate({opacity: "1"}, 'fast');
+            $(this).slideDown();
         });
         $("div div").addClass("change");
         $("div div").removeClass("changeback");
         count = count + 1;
     } else if(count == 1){
         $("nav").each(function(i){
-            $(this).animate({opacity: "0"}, 'fast');
+            $(this).slideUp();
         });
         $("div div").addClass("changeback");
         $("div div").removeClass("change");
